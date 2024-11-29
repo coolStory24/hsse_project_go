dcb63c7012426c882059d1bad2744c3a9cbe6377
FismanMaxim <mfisman05@gmail.com>
2024-11-29 20:24:18 +0300

    add env loading

--- hotel_service/go.mod
+++ hotel_service/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.9.0
 	gopkg.in/yaml.v3 v3.0.1
--- hotel_service/internal/app/app.go
+++ hotel_service/internal/app/app.go
@@ -1,10 +1,17 @@
 package app
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"hotel_service/internal/server"
 )
 
 func StartApp() {
+	err := loadEnv()
+	if err != nil {
+		panic(err)
+	}
+
 	cfg, err := server.NewCommonConfiguration()
 
 	if err != nil {
@@ -12,4 +19,12 @@ func StartApp() {
 	}
 
 	server.NewServer(cfg.ServerConfig, cfg.HotelService)
+}
+
+func loadEnv() error {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return fmt.Errorf("file .env in the root of the project not found")
+	}
+	return nil
 }
